823f7e4af4966bbfedba4b9be9efa3b734c6f166
crduque <crduque@hotmail.com>
2020-11-03 09:21:15 +0000

    solved problem, now cross button works

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -5,34 +5,29 @@ export const ToDoList = props => {
 	const [value, setValue] = useState("");
 	const [tasksApiArray, setTasksApiArray] = useState([]);
 	const [myListElement, setMyListElement] = useState(null);
-	const [crossClass, setCrossClass] = useState("list-element");
 
 	const isTaskDone = indexToCrossOut => {
-		if (tasksApiArray[indexToCrossOut].done == false) {
-			tasksApiArray[indexToCrossOut].done = true;
-			setCrossClass("list-element text-danger");
-		} else {
-			tasksApiArray[indexToCrossOut].done = false;
-			setCrossClass("list-element");
-		}
-		// if (tasksApiArray[indexToCrossOut].done == true) {
-		// 	crossClass = " crossedText";
-		// 	console.log(crossClass);
-		// } else {
-		// 	crossClass = "";
-		// }
-		fetch("https://assets.breatheco.de/apis/fake/todos/user/jan", {
-			method: "PUT",
-			body: JSON.stringify(tasksApiArray),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		})
-			.then(response => response.json())
-			.then(answerUpload => {
-				console.log("Success: ", JSON.stringify(answerUpload));
-			});
+		setTasksApiArray(
+			tasksApiArray.map((task, index) => {
+				if (index == indexToCrossOut) {
+					task.done = !task.done;
+				}
+				return task;
+			})
+		);
 	};
+
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/jan", {
+		method: "PUT",
+		body: JSON.stringify(tasksApiArray),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+		.then(response => response.json())
+		.then(answerUpload => {
+			console.log("Success: ", JSON.stringify(answerUpload));
+		});
 
 	useEffect(() => {
 		fetch("https://assets.breatheco.de/apis/fake/todos/user/jan", {
@@ -45,9 +40,6 @@ export const ToDoList = props => {
 				return response.json();
 			})
 			.then(responseAsJson => {
-				// responseAsJson.map((task, index) => {
-				// 	setTasksApiArray(task => [...tasksApiArray, task]);
-				// });
 				setTasksApiArray(responseAsJson);
 			})
 			.catch(error => {
@@ -74,6 +66,10 @@ export const ToDoList = props => {
 
 			setMyListElement(
 				tasksApiArray.map((task, index) => {
+					let crossClass = "list-element";
+					if (task.done) {
+						crossClass += " crossedText";
+					}
 					return (
 						<li key={index} className={crossClass}>
 							{task.label}
